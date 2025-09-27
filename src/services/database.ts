ec501b05ffa6069b248ced406e289af87808576b
Adam Ellis <adam.ellis.programmer@gmail.com>
2025-09-27 17:43:47 +0100

    database service start

---
+++ src/services/database.ts
@@ -0,0 +1,19 @@
+const databaseService = {
+  async getDocs() {
+    // ....
+  },
+  async getDoc() {
+    // ....
+  },
+  async createDoc() {
+    // ....
+  },
+  async updateDoc() {
+    // ....
+  },
+  async deleteDoc() {
+    // ....
+  },
+}
+
+export default databaseService
